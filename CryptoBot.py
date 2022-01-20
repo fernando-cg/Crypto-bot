4bc2060c18b3ca554155c777f1a750bf0847e74c
fernando <72653561+fernando-cg@users.noreply.github.com>
2022-01-20 12:42:29 +0100

    Update CryptoBot.py

--- CryptoBot.py
+++ CryptoBot.py
@@ -6,6 +6,7 @@ import telegram
 import telegram.ext as tl
 
 token = '5068805907:AAHdF-5G54qJ24HiZe2Lx9dSbz9XJzVQ4Us'
+#hacer que cada minuto se actualize la lista automaticamente para que no tarde en mostrar la lista 
 #Buscar una cripto en concreto Datos detallados iworPT
 
 #Lista de las 20 primeras criptos ordenadas por el precio total de la crypto
